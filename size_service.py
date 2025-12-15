ff8fe73c394805ebe65a8fc23da9fa088fac5c89
Carlos Yan <carlosyan2402@gmail.com>
2025-12-15 09:57:11 -0300

    Retirei algumas linhas desnecessárias.
    A versão anterior estava adequada para decodificar imagens que chegassem no formato de números binários.

--- size_service.py
+++ size_service.py
@@ -1,14 +1,14 @@
 #MOSTRA TAMANHO DA IMAGEM
 import cv2 as cv
-import numpy as np
 
 def size(img):
     try:
         if img is None:
             print("Error 404")
         else:
-            decoded_img = cv.imdecode(np.frombuffer(img, np.uint8), cv.IMREAD_COLOR)
-            img_height_lenght = decoded_img.shape[:2]
+            read_img = cv.imread(img)
+            img_height_lenght = read_img.shape[:2]
             return img_height_lenght
     except cv.Error as e:  
+
         print(e)
